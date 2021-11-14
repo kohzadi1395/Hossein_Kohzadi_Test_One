dea840e90647df4730d72b5d5aee67720e2b0fba
kohzadi1395 <kohzadi_hossein@yahoo.com>
2021-11-14 02:27:11 -0500

    addBook api

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
 const books = [
-	{
-		"authorName": "Hossein",
-		"title": "Node.js",
-		"publisher": "Lambton",
-		"subject": "Web",
-		"datePublished": "2021/10/19"
-	},
-	{
-		"authorName": "Hossein",
-		"title": "Node.js II",
-		"publisher": "Lambton",
-		"subject": "Web Programing",
-		"datePublished": "2021/10/19"
-	}
+    {
+        "authorName": "Hossein",
+        "title": "Node.js",
+        "publisher": "Lambton",
+        "subject": "Web",
+        "datePublished": "2021/10/19"
+    },
+    {
+        "authorName": "Hossein",
+        "title": "Node.js II",
+        "publisher": "Lambton",
+        "subject": "Web Programing",
+        "datePublished": "2021/10/19"
+    }
 
 ];
 const express = require("express");
 const app = express();
-const Port=process.env.PORT || 3000;
-app.listen(Port,()=>{
-  console.log("Server is ok");
+const Port = process.env.PORT || 3000;
+app.listen(Port, () => {
+    console.log("Server is ok");
 });
-app.get('/greeting',(req,res)=> {
-	res.send({hi:'here'});
+app.get('/greeting', (req, res) => {
+    res.send({hi: 'here'});
 });
-app.get('/',(req,res)=> {
-	res.send({hi:'Sweetheart'});
-});
-
-app.get('/getBooks/:subject',(req,res,next)=>{
-const subject=req.params.subject;
-console.log(subject);
-const book=books.find(c=>c.subject===subject);
-if(!book)
-{
-	res.status(404).send('book not find');
-}
-else
-{
-	res.json(book);
-}
-/*for(let book of books){
-   if( book.subject===subject){
-	res.json(book);
-	return;
-      }
-    }*/
+app.get('/', (req, res) => {
+    res.send({hi: 'Sweetheart'});
 });
 
-app.put('/updateBook',(req,res)=>{
-const oldSubject=req.params.oldSubject;
-const newSubject=req.params.newSubject;
-for(let book of books){
-   if( book.subject===oldSubject){
-	book.subject=newSubject;
-	res.json(book);
-	return;
-      }
+app.get('/getBooks/:subject', (req, res, next) => {
+    const subject = req.params.subject;
+    console.log(subject);
+    const book = books.find(c => c.subject === subject);
+    if (!book) {
+        res.status(404).send('book not find');
+    } else {
+        res.json(book);
+    }
+    /*for(let book of books){
+       if( book.subject===subject){
+        res.json(book);
+        return;
+          }
+        }*/
+});
+
+app.put('/updateBook', (req, res) => {
+    const oldSubject = req.params.oldSubject;
+    const newSubject = req.params.newSubject;
+    for (let book of books) {
+        if (book.subject === oldSubject) {
+            book.subject = newSubject;
+            res.json(book);
+            return;
+        }
     }
 });
 
-app.post('/addBook',(req,res)=>{
-const newBook=req.body;
-books.push(newBook)
+app.post('/addBook', (req, res) => {
+    const schema = {
+		subject: Joi.string().min(3).required_
+    };
+    const newBook =Joi.validate(req.body,schema);
+    books.push(newBook)
 });
 
-app.get('/displayAllBooks',(req,res)=>{
-	res.json(books);
-	return;
+app.get('/displayAllBooks', (req, res) => {
+    res.json(books);
+    return;
 });
 
 
